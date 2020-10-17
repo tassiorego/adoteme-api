e37f4ddd968f0072ca77672dc50ed49356b19e35
Tassio Rego <tassiorego@gmail.com>
2020-10-17 00:05:39 -0300

    chore: create Pet service

---
+++ src/modules/pets/services/CreatePetService.ts
@@ -0,0 +1,39 @@
+import { injectable, inject } from 'tsyringe';
+import Pet from '@modules/pets/infra/typeorm/entities/Pet';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import ICreatePetDTO from '../dtos/ICreatePetDTO';
+import IPetsRepository from '../repositories/IPetsRepository';
+
+@injectable()
+class CreatePetService {
+  constructor(
+    @inject('UsersRepository')
+    private petsRepository: IPetsRepository,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
+  ) {}
+
+  public async execute({
+    name,
+    description,
+    age,
+    type,
+    sex,
+    size,
+  }: ICreatePetDTO): Promise<Pet> {
+    const pet = await this.petsRepository.create({
+      name,
+      description,
+      age,
+      sex,
+      size,
+      type,
+    });
+
+    await this.cacheProvider.invalidatePrefix('pets-list');
+
+    return pet;
+  }
+}
+
+export default CreatePetService;
