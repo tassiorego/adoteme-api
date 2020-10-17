c4b609bc3e0c9e097eb985534808f61ae2a839c8
Tassio Rego <tassiorego@gmail.com>
2020-10-17 00:03:48 -0300

    chore: model for Pet

---
+++ src/modules/pets/infra/typeorm/entities/Pet.ts
@@ -0,0 +1,39 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('pets')
+class Pet {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  age: number;
+
+  @Column()
+  size: string;
+
+  @Column()
+  type: string;
+
+  @Column()
+  sex: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Pet;
